071bfeadd6baef689432112fb1765bfd4d6de060
wangshunlai <wanghuilongsd@163.com>
2020-10-10 18:38:15 +0800

    init file list api

--- server.js
+++ server.js
@@ -3,16 +3,26 @@ const Koa = require('koa');
 const mount = require("koa-mount");
 const Router = require('koa-router');
 const app = new Koa();
-
-
 const router = new Router();
+const fs = require('fs');
+
+const root = "./public"
+
 //route
 router.get('/ListDir', (ctx, next) => {
     // ctx.router available
-    ctx.request.query.dir
+    let files = []
+    innerFiles = fs.readdirSync(root, { withFileTypes: true })
+    for (let f in innerFiles) {
+        console.log(innerFiles[f].name)
+        files.push({ name: innerFiles[f].name, dir: innerFiles[f].isDirectory() })
+    }
+    console.log(files)
+    ctx.response.body = JSON.stringify(files)
 });
+
 //Static file path
-app.use(mount("/public", serve("./public"))).use(router.routes());
+app.use(mount("/public", serve(root))).use(router.routes());
 
 app.listen(3000)
 
